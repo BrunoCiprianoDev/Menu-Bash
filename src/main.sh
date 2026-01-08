dbd49b01abf8327cbac152f00c85e5cded41e708
BrunoCiprianoDev <cipriano990@gmail.com>
2026-01-07 21:45:20 -0300

    feature: create app laucher

--- src/main.sh
+++ src/main.sh
@@ -4,18 +4,18 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$BASE_DIR/lib/battery.sh"
 source "$BASE_DIR/lib/system_monitor.sh"
+source "$BASE_DIR/lib/app_launcher.sh"
 source "$BASE_DIR/lib/datetime/datetime_menu.sh"
 source "$BASE_DIR/lib/network_options/network_menu.sh"
 
 options=(
-    "Battery status"
+    "App launcher"
     "System status"
+    "Battery status"
     "Network manager"
     "Set Date & Time"
     "Exit"
 )
-
-selected=0
 
 GREEN_BG="\e[42m"
 BLACK_FG="\e[30m"
@@ -26,7 +26,7 @@ show_cursor() { tput cnorm; }
 trap show_cursor EXIT
 hide_cursor
 
-draw_menu() {
+draw_main_menu() {
     clear
     echo "==== Bash System Menu ===="
     echo "Time: $(date '+%d-%m-%Y %H:%M:%S')"
@@ -41,28 +41,36 @@ draw_menu() {
     done
 }
 
-while true; do
-    draw_menu
+render_main_menu() {
+    selected=0
 
-    read -rsn1 key
-    case "$key" in
-        $'\e')
-            read -rsn2 key
-            case "$key" in
-                '[A') ((selected--));;
-                '[B') ((selected++));;
-            esac
-            ((selected < 0)) && selected=$((${#options[@]} - 1))
-            ((selected >= ${#options[@]})) && selected=0
-            ;;
-        "")
-            case $selected in
-                0) battery_status ;;
-                1) system_monitor ;;
-                2) network_menu ;;
-                3) datetime_menu ;;
-                4) clear; exit ;;
-            esac
-            ;;
-    esac
-done
+    while true; do
+        draw_main_menu
+
+        read -rsn1 key
+        case "$key" in
+            $'\e')
+                read -rsn2 key
+                case "$key" in
+                    '[A') ((selected--)) ;;
+                    '[B') ((selected++)) ;;
+                esac
+                ((selected < 0)) && selected=$((${#options[@]} - 1))
+                ((selected >= ${#options[@]})) && selected=0
+                ;;
+            "")
+                case $selected in
+                    0) app_launcher ;;
+                    1) system_monitor ;;
+                    2) battery_status ;;
+                    3) network_menu ;;
+                    4) datetime_menu ;;
+                    5) clear; exit ;;
+                esac
+                ;;
+        esac
+    done
+}
+
+# MAIN - FUNCTION
+render_main_menu
